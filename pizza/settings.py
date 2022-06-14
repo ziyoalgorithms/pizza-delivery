05c8f96bd1d2879944b24b2b23ca0f9b33d48202
ziyoalgorithms <104044256+ziyoalgorithms@users.noreply.github.com>
2022-06-14 14:56:45 -0400

    update settings.py

--- pizza/settings.py
+++ pizza/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from decouple import config
 import dj_database_url
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -153,7 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=BASE_DIR / 'staticfiles'
+STATIC_ROOT=os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
