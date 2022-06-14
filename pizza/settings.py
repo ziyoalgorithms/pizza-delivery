86b42e437abc17e39a1cd1d016ccd91ef4b2bc65
Ziyodullokhon <studiovisual0077@gmail.com>
2022-06-14 18:47:43 -0400

    JWT Authentication with Djoser 8

--- pizza/settings.py
+++ pizza/settings.py
@@ -154,7 +154,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
